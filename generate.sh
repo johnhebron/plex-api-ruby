d55843cf7fea5d27117e1b74a2c2f4f91139f3cf
John Hebron <johnhebron@gmail.com>
2024-01-20 23:54:25 -0600

    updating GENERATOR_CLI_JAR to match workflow

--- generate.sh
+++ generate.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Path to the openapi-generator-cli JAR file
-GENERATOR_CLI_JAR="./openapi-generator-cli.jar"
+GENERATOR_CLI_JAR="./tmp/openapi-generator-cli.jar"
 
 # URL of the OpenAPI spec
 SPEC_URL="https://github.com/LukeHagar/plex-api-spec/raw/main/plex-media-server-spec-dereferenced.yaml"
